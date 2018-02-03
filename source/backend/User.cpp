5e8c8899ec87246aae102ba9b923469c984f3f68
SChoiGitHub <think88act@gmail.com>
2018-02-03 13:49:49 -0600

    Implemented getter methods.

--- source/backend/User.cpp
+++ source/backend/User.cpp
@@ -2,3 +2,15 @@
 /*
  * TO BE IMPLEMENTED BY: DOUG
  */
+User::User(){
+	
+}
+User::User(std::string uid, std::string pnm){
+
+}
+std::string User::get_user_name(){
+	return user_name;
+}
+std::string User::get_real_name(){
+	return real_name;
+}
