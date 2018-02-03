14f2ef778dcb6a12790f67b8f0692898a3d5a91d
SChoiGitHub <think88act@gmail.com>
2018-02-03 17:51:35 -0600

    Created and tested generate_event method and added the beginnings of the constructor.

--- source/backend/Executive.cpp
+++ source/backend/Executive.cpp
@@ -1,10 +1,23 @@
 #include "Executive.h"
 
 Executive::Executive(){
+	//Initialization of objects
 	event_list = new std::vector<Event>();
 	
+	if(!does_file_exist(df_event_list)){
+		std::ifstream create_directory_and_file("./data/EventList.txt");
+		create_directory_and_file.close();
+	}
+			
+	//Rebuild all information from .txt files
+	for(auto&& it : directory_iterator(boost::filesystem::path("./data/"))){
+		//This iterates over every file in the directory
+		//The name of the file is accessed with it.filename()
+	}
 }
 Executive::~Executive(){
+	
+	
 	delete event_list;
 }
 int Executive::get_event_num(){
@@ -15,19 +28,30 @@ int Executive::get_event_num(){
 bool Executive::generate_event(std::string name, std::string date){
 	try{		
 		//Create the event at the back of the vector.
-		event_list->push_back(Event(name,current_user.get_real_name(),date,get_event_num()));
+		event_list->push_back(Event(name,date,current_user.get_real_name(),get_event_num()));
+		
+		//This is a reference to the event we want to input data for.
+		Event &new_event = event_list->back();
 		
 		//Using the event at the back of the list (that we just created), we create the info and record files that correspond to it.
 		std::ofstream file_info_writer(
 			"./data/info_"
-			+ (event_list->back().get_id_number())
+			+ std::to_string((new_event.get_id_number()))
 			+ std::string(".txt")
 			);
 		std::ofstream file_record_writer(
 			"./data/record_"
-			+ (event_list->back().get_id_number())
+			+ std::to_string((new_event.get_id_number()))
 			+ std::string(".txt")
 			);
+			
+		//Now, we write the basic information of the event to the file in question.
+		file_info_writer << new_event.get_name() << '\n' << new_event.get_date() << '\n' << new_event.get_creator() << '\n' << new_event.get_event_num();
+		
+		//Close the files.
+		file_info_writer.close();
+		file_record_writer.close();
+		
 		return true;
 	}catch(std::string m){
 		return false;
@@ -38,12 +62,15 @@ bool Executive::does_file_exist(DataFile type, std::string identifer){
 	std::string file_name = "./data/"; 
 	
 	//Add the "prefix" of the file.
-	switch(type){
+	switch(df_event_list){
 		case df_event:
 			file_name+="info_";
 		break;
 		case df_user:
 			file_name+="user_";
+		break;
+		case df_event_list:
+			file_name+="EventList";
 		break;
 	}
 	
@@ -53,3 +80,4 @@ bool Executive::does_file_exist(DataFile type, std::string identifer){
 	//Returns whether a file exists or not.
 	return (boost::filesystem::exists(file_name));
 }
+
