29ad52a4431299bc605095352b58b8ce6638290e
SChoiGitHub <think88act@gmail.com>
2018-02-03 09:03:36 -0600

    Added missing skeleton methods.

--- source/backend/Record.h
+++ source/backend/Record.h
@@ -4,7 +4,16 @@
 
 class Record{
 	public:
+		Record();
+		Record(int in_id, std::string t);
+		~Record();
+		
+		void add_user(User u);
+		void remove_user(User u);
 	private:
+		std::string time;
+		//list of users
+		//int id; //may not need this
 };
 
 #endif
