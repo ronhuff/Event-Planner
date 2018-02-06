a64455ffe015840bf8b5af705d806b0d9bacf7db
XZhou <x534z011@ku.edu>
2018-02-06 11:30:09 -0600

    The Record Class
    Please check my work, I don't know if I using the list correct.

--- source/backend/Record.h
+++ source/backend/Record.h
@@ -1,18 +1,25 @@
 #ifndef RECORD_H
 #define RECORD_H
 
+#include "User.h"
+
+#inlcude <string>
+#include <list>
+
 
 class Record{
 	public:
 		Record();
-		Record(int in_id, std::string t);
+		Record(std::string t);
 		~Record();
 		
-		void add_user(User u);
-		void remove_user(User u);
+		void add_user(User uid);
+		void remove_user(User uid);
+        std:list<User> getUserList();
+    
 	private:
 		std::string time;
-		//list of users
+        std::list<User> UserList;
 		//int id; //may not need this
 };
 
