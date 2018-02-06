c63c092c2602c1d82ea25861f539d51c08aa5378
SChoiGitHub <think88act@gmail.com>
2018-02-06 15:26:53 -0600

    Modified main.cpp to become a tester program.

--- source/backend/main.cpp
+++ source/backend/main.cpp
@@ -3,30 +3,26 @@
 
 int main(int argc, char* argv[]){
 	Executive e;
-	switch(argv[1][0]){
-		case 'a':
-			if(argc == 4){
-				std::cout << e.generate_event(argv[2],argv[3]) << '\n';
-			}
-		break;
-		case 'r':
-			if(argc == 3){
-				std::cout << e.delete_event(std::stoi(argv[2])) << '\n';
-			}
-		break;
-		case 'd':
-			std::vector<Event> l = e.get_event_list();
-			for(auto&& x = l.begin(); x != l.end(); x++){
-				//Iterate over every element in the vector.
-				std::cout << x->get_id_number() << '\n';
-			}
-		break;
-		default:
-			std::cout << "Welcoming to testing. Extend the main file as needed.\n";
-			std::cout << "If the first argument is 'a', give an event name and a date as the 2nd and 3rd argument to add an event. A '1' indicates sucess.\n"
-			std::cout << "If the first argument is 'd', display all events in file.\n"
-			std::cout << "If the first argument is 'r', give an integer to delete the event with the id number. A '1' indicates sucess.\n"
-			
+	if(argc <= 2){
+		switch(argv[1][0]){
+			case 'a':
+				if(argc == 4){
+					std::cout << e.generate_event(argv[2],argv[3]) << '\n';
+				}
+			break;
+			case 'r':
+				if(argc == 3){
+					std::cout << e.delete_event(std::stoi(argv[2])) << '\n';
+				}
+			break;
+			case 'd':
+				std::vector<Event> l = e.get_event_list();
+				for(auto&& x = l.begin(); x != l.end(); x++){
+					//Iterate over every element in the vector.
+					std::cout << x->get_id_number() << '\n';
+				}
+			break;
+		}
 	}
 }
 
