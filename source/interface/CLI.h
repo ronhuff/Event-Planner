179205aab88a97e98a8526c4f9038db870a4f5e8
William Graham <willgraham@live.com>
2018-02-09 16:12:17 -0600

    Initial layout of CLI (Command Line Interface) class header.

---
+++ source/interface/CLI.h
@@ -0,0 +1,24 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <iostream>
+#include <string>
+#include "Executive.h"
+
+class CLI{
+    private:
+        Executive exec;
+        enum EventSet{all, attending, created};
+    public:
+        void run();
+    private:
+        void login();
+        void logout();
+        void newAccount();
+        void listEvents(EventSet, int);
+        void newEvent();
+        void viewEvent(int);
+        void setAvailability();
+};
+
+#endif
