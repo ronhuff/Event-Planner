106d975cb7f113bd4459fec1a2594da32da30867
William Graham <willgraham@live.com>
2018-02-09 16:16:00 -0600

    Start of login functionality for application

--- source/interface/CLI.cpp
+++ source/interface/CLI.cpp
@@ -3,3 +3,20 @@
 CLI::CLI(){
     exec = new Executive();
 }
+
+/*Logs a user into the application*/
+CLI::login(){
+
+    while(!loggedin){
+        std::string identifier;
+        std::cout << "You need to login to continue. If you do not have an account enter \"CreateAccount\".\nEnter your username: ";
+        std::cin >> identifier;
+
+        if(identifier == "CreateAccount"){
+            newAccount();
+        }else{
+            //Attempt Login
+            if()
+        }
+    }
+}
