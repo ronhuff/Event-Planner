b1d07f86750d21a774c8caaf3a1835db996ede12
Doug Newman <douglasnewman987@gmail.com>
2018-02-11 11:30:36 -0600

    Fixed interface testing file

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,8 @@
-#include "./backend/Executive.h"
+#include "interface/CLI.h"
 
 int main(int argc, char* argv[]){
-	Executive* e = new Executive();
-	e->setCurrentUser("abba");
-	e->generateEvent("John Cena's Party", "2002-1-25");
-	if(argc == 2){
-		Event* a = e->getEventByID(std::stoi(argv[1]));
-		std::cout << a->getDate() << '\n';
-		std::cout << a->getCreatorUserName() << '\n';
-		std::cout << a->getName() << '\n';
-		delete a;
-	}
-	
+    CLI interface;
+    interface.run();
+
+    return (0);
 }
