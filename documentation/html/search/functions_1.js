15e91e42fba7d64a9886ee698254abacc91f6463
SChoiGitHub <think88act@gmail.com>
2018-02-12 07:28:13 -0600

    Updated Documentation

--- documentation/html/search/functions_1.js
+++ documentation/html/search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['cli',['CLI',['../classCLI.html#a1a5b0f7ec3b242c5c56e4745adbb391e',1,'CLI']]],
+  ['cliinput',['CLIInput',['../classCLIInput.html#a1c034047988b07e0dc3c91509c9582bc',1,'CLIInput']]],
   ['createrecordlist',['createRecordList',['../classExecutive.html#a513c12461670a2a2c6b5f69a387fa43c',1,'Executive']]],
   ['createuser',['createUser',['../classExecutive.html#aaf4e73a2e3e6515a1e15569d50ed7017',1,'Executive']]]
 ];
--- documentation/html/search/functions_b.js
+++ documentation/html/search/functions_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['setavailability',['setAvailability',['../classCLI.html#ab554ae888dbb87763eaa8d15fbd75bfb',1,'CLI']]],
+  ['setavailability',['setAvailability',['../classCLI.html#a0546e36efd50af1c80eee2ed47ac4f12',1,'CLI']]],
   ['setcurrentuser',['setCurrentUser',['../classExecutive.html#a8010517a6dd4bbe81ce4a03a91886931',1,'Executive']]],
   ['sorteventlist',['sortEventList',['../classExecutive.html#a63e7fcccc1e3992eaef93630ecc2a160',1,'Executive']]],
   ['storeintsfromstring',['storeIntsFromString',['../classExecutive.html#a71b1545d70bafaee9d59ef90c6af97cf',1,'Executive']]]
