6c016946c7965775614960eef42177e75361e579
Ron Huff <ron.huff@ku.edu>
2018-03-01 16:00:34 -0600

    Added TaskList object to event & addTaskList()

--- EventPlanner/backend/Task.h
+++ EventPlanner/backend/Task.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <string>
-#include "backend/User.h"
+#include "User.h"
 
 class Task
 {
 public:
 	Task();
-	Task(std::string name);
+	Task(std::string name); //creates task and sets m_name = name
 	~Task();
 
 	bool assignTask(User*); //Assigns a user to m_taskUser
-	bool m_isAssigned;
+	bool m_isAssigned; //True if task is assigned, else false.
 	std::string m_name; //The name of the task, e.g. "Bring Chips."
 
 	User* m_taskUser; //Points to the user that has accepted this task.
--- EventPlanner/backend/TaskList.h
+++ EventPlanner/backend/TaskList.h
@@ -13,11 +13,14 @@ public:
 	~TaskList();
 
 	bool addTask(Task*);
-	bool createTask();
+	bool createTask(); // Gets name of task from user and attempts to call addTask(new Task(name));
 
 	int m_numTasks;
 	int m_numAvailTasks;
-	std::vector<Task*>* m_tasks;
+	std::vector<Task*>* m_tasks; // contains all tasks related to this task list.
+
+	//ADD VIEW METHODS HERE, e.g. viewTasks(bool isCreator);
+
 private:
 
 	
